43681ae4acaf1a8e2ee3132b23fafc74941c7f47
Yazmin <moyanoyazminalejandra@gmail.com>
2024-04-10 21:04:52 -0300

    fin apartado c

--- tp2_4.c
+++ tp2_4.c
@@ -11,6 +11,7 @@ struct compu {
 } typedef compu;
 
 void cargarArregloPC(compu *p,int cant);
+void mostrarArregloPC(compu *p,int cant);
 
 
 int main(){
@@ -20,22 +21,37 @@ int main(){
     puntero = arregloPC;
 
     cargarArregloPC(puntero,MAX);
+    mostrarArregloPC(puntero,MAX);
 
     return 0;
 
 }
 
-void cargarArregloPC(compu *p,int cant)
-{
+void cargarArregloPC(compu *p,int cant) {
     srand (time(NULL));
     char *tipos[6] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
 
-    for (int i = 0; i < cant; i++)
-    {
+    for (int i = 0; i < cant; i++) {
         p->velocidad = rand() % 3 + 1;
         p->anio = rand() % 9 + 2015 ;
         p->cantidad = rand() % 8 + 1;
         p->tipo_cpu = tipos[rand() % 6];
+
+        p++;
+    }
+}
+
+// Función que presente la lista de las PC, cada una con sus características.
+void mostrarArregloPC(compu *p,int cant){
+
+    printf("\nLISTA DE LAS PC:");
+    for (int i = 0; i < cant; i++) {
+
+        printf("\n\nCARACTERISTICAS PC [%d]", i + 1);
+        printf("\nVelocidad de procesamiento en GHz: %d", p->velocidad);
+        printf("\nAnio de fabricacion: %d", 164, p->anio );
+        printf("\nTipo de procesador: %s", p->tipo_cpu );
+        printf("\nCantidad de nucleos; %d",163, p->cantidad);
 
         p++;
     }
